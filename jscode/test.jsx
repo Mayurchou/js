6c5957d7e300112e873b21d0d78eda3b1ec9407e
Mayur choudhary <108649101+Mayurchou@users.noreply.github.com>
2025-08-01 17:32:37 +0000

    Refactor: Comment out additional unused code in test.jsx

--- jscode/test.jsx
+++ jscode/test.jsx
@@ -48,11 +48,56 @@
 
 
 
-let meg = "Global message";
+// let meg = "Global message";
 
-function showMessge(){
-    let meg = "Local message";
-    console.log(meg); // This will log "Local message"
-}
-showMessge();
-// console.log(meg); // This will log "Global message"
+// function showMessge(){
+//     let meg = "Local message";
+//     console.log(meg); // This will log "Local message"
+// }
+// showMessge();
+// // console.log(meg); // This will log "Global message"
+
+
+
+// {
+//     var x = 10; // Global variable
+//     let y = 20; // Block-scoped variable
+//     const z = 30; // Block-scoped constant
+// }
+// console.log(x); // This will log 10
+// // console.log(y); // This will throw an error because 'y' is not defined outside the block
+// // console.log (z); // This will throw an error because 'z' is not defined outside the block
+
+
+
+
+// var a = 10;
+// var a = 20; // This is valid, 'a' is redeclared
+
+// console.log(a); // This will log 20
+
+// let b = 10;
+// // let b = 20; // This will throw an error because 'b' is already declared
+// console.log(b); // This line will not be executed due to the error above
+
+// const c = 10;
+// // const c = 20; // This will throw an error because 'c' is already declared
+// console.log(c); // This line will not be executed due to the error above
+
+
+// loopScoping 
+
+for (var i = 0; i < 3; i++){
+    setTimeout (()  => 
+        console.log('var i: ', i), 1000);
+    }
+
+
+for (let j = 0; j <3; j++){
+    setTimeout (() => 
+        console.log('let j',j ),1000)
+}    
+
+
+
+
