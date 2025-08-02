c54c774c70557f1dae7008f9e5b30ea8df14f1d6
Mayur choudhary <108649101+Mayurchou@users.noreply.github.com>
2025-08-02 17:45:55 +0000

    Refactor: Comment out additional code examples in test.jsx

--- jscode/test.jsx
+++ jscode/test.jsx
@@ -87,17 +87,121 @@
 
 // loopScoping 
 
-for (var i = 0; i < 3; i++){
-    setTimeout (()  => 
-        console.log('var i: ', i), 1000);
-    }
+// for (var i = 0; i < 3; i++){
+//     setTimeout (()  => 
+//         console.log('var i: ', i), 1000);
+//     }
 
 
-for (let j = 0; j <3; j++){
-    setTimeout (() => 
-        console.log('let j',j ),1000)
-}    
+// for (let j = 0; j <3; j++){
+//     setTimeout (() => 
+//         console.log('let j',j ),1000)
+// }    
 
 
 
 
+// Tranditional Function Declaration
+
+// function add(a, b ){
+//    return a + b;
+// }
+
+// add(10,5); // This will return 15
+
+
+// arrow function
+
+// let add = (a, b) => {
+//     return a + b;
+// }
+
+// add(10, 5); // This will return 15
+
+
+
+
+// function add(a,b){
+//     return a +b;
+
+// }
+// add (11,20); // This will return 31
+
+
+
+
+// // 1 No parameter
+// const greet = () => {
+//     console.log('hello world from arrow function');
+// }
+// greet(); // This will log "hello world from arrow function"
+
+
+// // 2 Single parameter
+// const grretuser = name => {
+//     name = name || 'undefined'; // 
+//     console.log(`hello ${name}, welcome to arrow function`);
+// }
+
+// grretuser('Mayur'); // This will log "hello undefined, welcome to arrow function"
+
+
+// // 3 Multiple parameters
+// const multiply = (a,b) => {
+//     return a * b;
+
+// }
+// console.log(multiply(5, 10)); // This will log 50
+
+
+// //4. With curly braces and return
+
+// const devide = (a,b) => {
+//     return a / b;
+// }
+// console.log(devide(10, 2)); // This will log 5
+
+
+
+
+// Callback Function
+
+
+// function greet(name, callback){
+//     console.log(`Hello, ${name}!`);
+//     callback();
+// }
+
+// function Goodnbye(){
+//     console.log('Goodbye!');
+// }
+// greet('Mayur', Goodnbye); // This will log "Hello, Mayur!" followed by "Goodbye!"
+
+
+// Simulating server request
+
+// function fetchData(callbacks){
+//     console.log('Fetching data.......');{
+//         setTimeout(() => {
+//             const data = { id: 1 , name : 'Mayur' };
+//             console.log('Data fetched successfully');
+//         })
+//     }
+// }
+// fetchData(() => {
+//     console.log('Data is ready to be processed');
+// });
+
+
+// ✅ Arrow Function with Callback
+
+function fetchData(Callback){
+    console.log('Fetching data......');
+    setTimeout (() => {
+        const data = { id : 1, name: 'Mayur '};
+        console.log('Data is fetched successfully');
+        Callback(data);
+            })}
+fetchData((data) => { 
+    console.log('Data is ready to be processed:', data);
+});
